e38c564a8779bfb113ed53857d3642f81b50bee5
GabrielSBrito17 <gsbbrito@hotmail.com>
2026-09-22 23:13:30 +0100

    vercel com keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,16 @@ import tailwindcss from '@tailwindcss/vite';
 import keystatic from '@keystatic/astro';
 import markdoc from '@astrojs/markdoc';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   integrations: [keystatic(), markdoc()],
+
   vite: {
     plugins: [tailwindcss()],
   },
-  output: 'hybrid',
+
+  adapter: vercel(),
 });
